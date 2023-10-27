7cc1c88c4f35250225ddeb1a8551a94d8c9e7e6a
bescoss <pablo.bescos@sarria.salesians.cat>
2023-10-27 16:37:44 +0200

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,69 +1,34 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Hello World Pablo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightGreen),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Hello, World!'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  void _incrementCounter() {
-    setState(() {});
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-          title: Text(widget.title),
-        ),
-        body: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Container(
-                  color: Colors.red,
-                  child: Text(
-                    'One',
-                  )),
-              Container(
-                  color: Colors.blue,
-                  child: Text(
-                    'Two',
-                  )),
-              Container(
-                  color: Colors.green,
-                  child: Text(
-                    'Three',
-                  )),
-              Text(
-                'PABLO BESCOS 23-24 SSARRIÀ',
-                textAlign: TextAlign.center,
-              )
-            ]));
-  }
+  return runApp(MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.blue,
+            title:
+                const Text("Layout App", style: TextStyle(color: Colors.white)),
+          ),
+          body: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Container(
+                    color: Colors.red,
+                    child: const Text(
+                      'One',
+                    )),
+                Container(
+                    color: Colors.blue,
+                    child: const Text(
+                      'Two',
+                    )),
+                Container(
+                    color: Colors.green,
+                    child: const Text(
+                      'Three',
+                    )),
+                const Text(
+                  'PABLO BESCOS 23-24 SSARRIÀ',
+                )
+              ]))));
 }
